d922fc689b148f3dcbc9693e563564ecf2826f82
Andi Abdul Salam Sabil <58612952+aabdulsalams@users.noreply.github.com>
2022-10-12 10:45:22 +0700

    Update read-collection.sh

--- read-collection.sh
+++ read-collection.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+# check required tools
+if !hash newman 2>/dev/null; then
+    echo  '`newman` not installed.'
+    exit 1
+fi
+if !hash jq 2>/dev/null; then
+    echo  '`jq` not installed. Needed for parsing JSON.'
+    exit 1
+fi
+if !hash curl 2>/dev/null; then
+    echo  '`curl` not installed. Needed for syncing with Postman.'
+    exit 1
+fi
+
+# convert newenv for update env in postman
 cat newenv.json | jq -r '{ "environment": { "name": .name|tostring,
     "values": [
     .values[] | {
