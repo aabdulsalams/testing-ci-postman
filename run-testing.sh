af8ede643007d3ecba294fd9b3fffb2283a888bb
Andi Abdul Salam Sabil <58612952+aabdulsalams@users.noreply.github.com>
2022-10-12 11:08:35 +0700

    Update and rename read-collection.sh to run-testing.sh

--- run-testing.sh
+++ run-testing.sh
@@ -14,6 +14,20 @@ if !hash curl 2>/dev/null; then
     exit 1
 fi
 
+# check parameter input
+if [[ -z "$1" ]]; then
+    echo 'No api key found. => bash collection.sh [API_KEY] [COLLECTION_ID] [ENVIRONMENT_ID]'
+    exit 1
+fi
+if [[ -z "$2" ]]; then
+    echo 'No collection ID. => bash collection.sh [API_KEY] [COLLECTION_ID] [ENVIRONMENT_ID]'
+    exit 1
+fi
+if [[ -z "$3" ]]; then
+    echo 'No environment ID. => bash collection.sh [API_KEY] [COLLECTION_ID] [ENVIRONMENT_ID]'
+    exit 1
+fi
+
 # convert newenv for update env in postman
 cat newenv.json | jq -r '{ "environment": { "name": .name|tostring,
     "values": [
@@ -23,3 +37,8 @@ cat newenv.json | jq -r '{ "environment": { "name": .name|tostring,
         "type": .value|type
     }
 ] } }' > updatedenv.json
+
+# Run Automation
+collection_url="https://api.getpostman.com/collections/$2?apikey=$1"
+environment_url="https://api.getpostman.com/environments/$3?apikey=$1"
+
